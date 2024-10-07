ece48ef11d8224495a4e3c4a313dcb75f78c3d35
Aloysius <aloyziuz@users.noreply.github.com>
2024-10-07 17:35:21 +0700

    refactored code into a class

--- PostgreSQLHelper.js
+++ PostgreSQLHelper.js
@@ -1,8 +1,69 @@
 const pg = require("pg");
-const fs = require("fs");
 const conf = require("config");
+
+class PGHelper{
+    constructor(config){
+        this.pool = new pg.Pool(config);
+    }
+
+    /**
+	* gets pg nodule used by the helper
+	*/
+	GetPG(){
+		return pg;
+	}
+    /**
+     * executes a single query and returns the result
+     * @param {string} sql 
+     * @param {any[]} args 
+     */
+	async ExecuteSingleQuery(sql, args){
+		var res = await this.pool.query(sql, args);
+		return res;
+    }
+    
+    /**
+     * starts a new transaction. CommitTransaction or RollbackTransaction has to be called to end this transaction
+     * @returns {Promise<pg.PoolClient>}
+     */
+	async StartTransaction(){
+        var client = await this.pool.connect();
+        await client.query("BEGIN");
+		return client;
+	}
+    
+    /**
+     * will commit all changes in this transaction
+     * @param {pg.PoolClient} dbcon 
+     */
+	async CommitTransaction(dbcon){
+		await dbcon.query("COMMIT");
+		await dbcon.release();
+    } 
+    
+    /**
+     * will undo all operations in this transaction
+     * @param {pg.PoolClient} dbcon 
+     */
+	async RollbackTransaction(dbcon){
+		await dbcon.query("ROLLBACK");
+		await dbcon.release();
+	}
+    
+    /**
+     * Perform an sql query in a transaction
+     * @param {pg.PoolClient} dbcon 
+     * @param {string} sql 
+     * @param {any[]} args 
+     */
+	async ExecuteQuery(dbcon, sql, args){
+		var res = await dbcon.query(sql, args);
+		return res;
+	}
+}
+
 var dbconfig = conf.get("dbConfig");
-const pool = new pg.Pool({
+var pghelper = new PGHelper({
     user: dbconfig.username, 
     host: dbconfig.host, 
     database: dbconfig.db, 
@@ -10,12 +71,21 @@ const pool = new pg.Pool({
     port: dbconfig.port
 });
 
+
 module.exports = {
+    PGHelper: PGHelper,
+    /**
+     * Sets the config for the pg pool. Has to be called before any other method
+     * @param {import("pg").PoolConfig} conf - config for the pg pool
+     */
+    SetPG: function(conf){
+        pghelper = new PGHelper(conf);
+    },
 	/**
 	* gets pg nodule used by the helper
 	*/
 	GetPG: function(){
-		return pg;
+		return pghelper.GetPG();
 	},
     /**
      * executes a single query and returns the result
@@ -23,8 +93,7 @@ module.exports = {
      * @param {any[]} args 
      */
 	ExecuteSingleQuery: async function(sql, args){
-		var res = await pool.query(sql, args);
-		return res;
+		return await pghelper.ExecuteSingleQuery(sql, args);
     }, 
     
     /**
@@ -32,9 +101,7 @@ module.exports = {
      * @returns {Promise<pg.PoolClient>}
      */
 	StartTransaction: async function(){
-        var client = await pool.connect();
-        await client.query("BEGIN");
-		return client;
+        return await pghelper.StartTransaction();
 	}, 
     
     /**
@@ -42,8 +109,7 @@ module.exports = {
      * @param {pg.PoolClient} dbcon 
      */
 	CommitTransaction: async function(dbcon){
-		await dbcon.query("COMMIT");
-		await dbcon.release();
+		return await pghelper.CommitTransaction(dbcon);
     }, 
     
     /**
@@ -51,8 +117,7 @@ module.exports = {
      * @param {pg.PoolClient} dbcon 
      */
 	RollbackTransaction: async function(dbcon){
-		await dbcon.query("ROLLBACK");
-		await dbcon.release();
+		return await pghelper.RollbackTransaction(dbcon);
 	}, 
     
     /**
@@ -62,7 +127,6 @@ module.exports = {
      * @param {any[]} args 
      */
 	ExecuteQuery: async function(dbcon, sql, args){
-		var res = await dbcon.query(sql, args);
-		return res;
+		return await pghelper.ExecuteQuery(dbcon, sql, args);
 	}
 }
